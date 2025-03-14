66a5413dc830f19334b82e11a2b9a5fa59eedbf5
Phiwamandla Mkhwanazi <131719046+Phiwamandla-Mkhwanazi@users.noreply.github.com>
2025-03-14 15:17:41 +0200

    Update index.js: Encapsulation, Destructuring and Debugging
    1)  Encapsulation (OOP) :
    - Bundling the data (properties) and the methods (functions) that operate on the data into a single unit (the object). The object spacecraft is encapsulating the spacecraft's properties and methods that act on them.
    
    2) Destructuring: 
    -  Allows to extract properties and assign them directly to variables. This avoids having to repeatedly access properties like spacecraft.initialVelocity or spacecraft.finalDistance().
    
    3) Debugging:
    - The 'CalculateNewVelocity ' was declared a function inside the object 'spacecraft' hence we update '=' and  '=>' , as it will no longer be an anonymous function outside an object to ':' and 'function'.
    
    #function(){}, allows the use of 'this' keyword while anonymous function don't.

--- index.js
+++ index.js
@@ -6,27 +6,48 @@
  * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
  */
 
-// Given Parameters
-const initialVelocity = 10000; // velocity (km/h)
-const acceleration = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const initialDistance = 0; // distance (km)
-const initialFuel = 5000; // remaining fuel (kg)
-const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
+const spacecraft = 
+{
+  // Given Parameters
+  initialVelocity: 10000, // velocity (km/h)
+  acceleration: 3, // acceleration (m/s^2)
+  time: 3600, // seconds (1 hour)
+  initialDistance: 0, // distance (km)
+  initialFuel: 5000, // remaining fuel (kg)
+  fuelBurnRate: 0.5, // fuel burn rate (kg/s)
 
+  //Spacecraft functions
+  finalDistance: function()
+  {
+     return this.initialDistance + ( this.initialVelocity * this.time );//calcultes new distance
+  },
+  
+  remainingFuel: function()
+  {
+    return this.initialFuel - (this.fuelBurnRate * this.time); //calculates remaining fuel
+  },
+ 
+  finalVelocity: function()
+  {
+    return this.calculateNewVelocity();//calculates new velocity based on acceleration
+  }, 
 
-const finalDistance = initialDistance + ( initialVelocity * time ) //calcultes new distance
-const remainingFuel = fuelBurnRate * time //calculates remaining fuel
-const finalVelocity = calcNewVel(acceleration, initialVelocity, time) //calculates new velocity based on acceleration
-
-// Pick up an error with how the function below is called and make it robust to such errors
-calculateNewVelocity = (initialVelocity, acceleration, time) => { 
-  return initialVelocity + ( acceleration * time)
+  // Pick up an error with how the function below is called and make it robust to such errors
+  //This function is responsible for obtaining the initial velocity in m/s
+  calculateNewVelocity: function()
+  { 
+    return this.initialVelocity + ( this.acceleration * this.time);
+  }
 }
 
-console.log(`Corrected New Velocity: ${finalVelocity} km/h`);
-console.log(`Corrected New Distance: ${finalDistance} km`);
-console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
+// Destructuring
+const { finalVelocity, finalDistance, remainingFuel } = spacecraft;
+
+//Call Spacecraft functions to get more information
+console.log(Corrected New Velocity: ${finalVelocity()} km/h); 
+console.log(Corrected New Distance: ${finalDistance()} km);
+console.log(Corrected Remaining Fuel: ${remainingFuel()} kg);
+
 
 
 
