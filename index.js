592f815da74d1896a62313d5a75d8632866108fa
Phiwamandla Mkhwanazi <131719046+Phiwamandla-Mkhwanazi@users.noreply.github.com>
2025-03-14 14:34:22 +0200

    Update index.js: Refactor code to follow camelCase naming convention

--- index.js
+++ index.js
@@ -7,26 +7,26 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
+const initialVelocity = 10000; // velocity (km/h)
+const acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const initialDistance = 0; // distance (km)
+const initialFuel = 5000; // remaining fuel (kg)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const finalDistance = initialDistance + ( initialVelocity * time ) //calcultes new distance
+const remainingFuel = fuelBurnRate * time //calculates remaining fuel
+const finalVelocity = calcNewVel(acceleration, initialVelocity, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+calculateNewVelocity = (initialVelocity, acceleration, time) => { 
+  return initialVelocity + ( acceleration * time)
 }
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+console.log(`Corrected New Velocity: ${finalVelocity} km/h`);
+console.log(`Corrected New Distance: ${finalDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 
 
